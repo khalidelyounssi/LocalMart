85b5f81d567431ab8a93c7f76afe29d42b9fcd10
Khalid Elyounssi <elyounssiox2000@gmail.com>
2026-02-05 11:59:58 +0100

    feat: chnge status products

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function product()
+{
+    return $this->belongsTo(Product::class);
+}
+
+public function user()
+{
+    return $this->belongsTo(User::class);
+}
 }
